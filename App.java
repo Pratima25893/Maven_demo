354acd8d47b6ac70a970738f7fbd0b648437915f
Pratima25893 <52877732+Pratima25893@users.noreply.github.com>
2022-05-07 10:23:48 +0530

    App.java

---
+++ App.java
@@ -0,0 +1,39 @@
+package $package;
+
+#if ( $junitVersion == $null || $junitVersion.startsWith("4."))
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+#else
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+#end
